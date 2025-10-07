16bfbac0696fa5bf0748441e27e8055adf4abbb8
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2025-10-07 14:27:01 -0400

    navbar seems to be done, styling will be changed later on

---
+++ src/app/frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "../styles/Home.css"; // Optional: Add styles for the Home page
+
+export default function HomePage() {
+  return (
+    <div className="container">
+      <div className="splash">
+        <h1>Welcome to Buggin Out!</h1>
+        <p>Your go-to app for delicious recipes.</p>
+        <p>
+          Explore a variety of recipes from crunchy snacks to gourmet dishes.
+        </p>
+        <p>
+          Get started by browsing our <Link to="/recipes">Recipes</Link> page or
+          learn more <Link to="/about">About Us</Link>.
+        </p>
+      </div>
+    </div>
+  );
+}
