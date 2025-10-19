519c04c04695926f7d438bf25e53f7970806a98a
CSJLU <jlu@DESKTOP-0FN7PVT.localdomain>
2025-10-18 21:36:52 -0400

    Create route for Spoonacular API and successful data retrieval

--- src/app/backend/index.js
+++ src/app/backend/index.js
@@ -1,17 +1,32 @@
+// load api key from env
+require("dotenv").config();
+
+const cors = require("cors");
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-// Middleware to parse JSON
+// middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
-// Sample route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const SPOON_API_KEY = process.env.SPOON_API_KEY;
+
+// test, temporarily setting to default page
+app.get("/", async (req, res) => {
+  const url = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=${SPOON_API_KEY}`
+  try {
+    const response = await axios.get(url);
+    res.json(response.data); // display in browser
+  } catch (error) {
+    console.error("Error fetching data:", error.response?.data || error.message);
+    res.status(500).json({ error: "Failed to fetch from Spoonacular" });
+  }
 });
 
-// Start the server
+// start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
