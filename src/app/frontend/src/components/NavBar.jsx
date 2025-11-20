b6cc24b143366e25c8dbd021892697c41161a291
CSJLU <jlu@DESKTOP-0FN7PVT.localdomain>
2025-11-20 15:53:53 -0500

    Refine navbar

--- src/app/frontend/src/components/NavBar.jsx
+++ src/app/frontend/src/components/NavBar.jsx
@@ -1,36 +1,25 @@
 import { Link } from "react-router-dom";
-import "../styles/NavBar.css"; // Optional: Add styles for your NavBar
+import "../styles/NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <ul className="navbar-links">
-        <li>
-          <Link to="/">Forkcast</Link>
-        </li>
-        <ul className="right-links">
-          <li className="navbar-signup">
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-        <div className="divider">
-          <li id="pages">
-            <Link to="/recipes">Recipes</Link>
-          </li>
-          <li>
-            <Link to="/grocery">Grocery</Link>
-          </li>
-          <li>
-            <Link to="/macro">Macro Tracker</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </div>
-        {/* Add more links for other pages in the `pages` folder */}
+      <div className="nav-left">
+        <Link to="/" className="name">
+          Forkcast
+        </Link>
+      </div>
+
+      <ul className="nav-center">
+        <li><Link to="/recipes">Recipes</Link></li>
+        <li><Link to="/grocery">Grocery</Link></li>
+        <li><Link to="/macro">Macro Tracker</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+
+      <ul className="nav-right">
+        <li><Link to="/signup" className="btn signup">Sign Up</Link></li>
+        <li><Link to="/login" className="btn login">Login</Link></li>
       </ul>
     </nav>
   );
